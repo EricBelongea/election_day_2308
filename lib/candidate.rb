22558e45f27b82931e52f37df80a7f16d091f726
Eric Belongea <eric.belongea@gmail.com>
2023-09-14 15:28:08 -0600

    Setp up attributes and voting method

---
+++ lib/candidate.rb
@@ -0,0 +1,13 @@
+class Candidate
+  attr_reader :name, :party, :votes
+
+  def initialize(hash)
+    @name = hash[:name]
+    @party = hash[:party]
+    @votes = 0
+  end
+
+  def vote_for!
+    @votes += 1
+  end
+end
